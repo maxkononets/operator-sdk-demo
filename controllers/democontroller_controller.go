f9b46f25414eb5bbac8e49727376b42b3c236abc
Maksym Kononets <maksym.kononec@nixsolutions.com>
2022-04-18 17:02:53 +0300

    Implemented reconciliation logic for democontroller and generated rbac for such resource

--- controllers/democontroller_controller.go
+++ controllers/democontroller_controller.go
@@ -18,13 +18,18 @@ package controllers
 
 import (
 	"context"
-
+	groupcontrollerv1alpha1 "github.com/maxkononets/operator-sdk-demo/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"reflect"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	groupcontrollerv1alpha1 "github.com/maxkononets/operator-sdk-demo/api/v1alpha1"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
+	"time"
 )
 
 // DemoControllerReconciler reconciles a DemoController object
@@ -36,27 +41,149 @@ type DemoControllerReconciler struct {
 //+kubebuilder:rbac:groups=groupcontroller.hub.docker.com,resources=democontrollers,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=groupcontroller.hub.docker.com,resources=democontrollers/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=groupcontroller.hub.docker.com,resources=democontrollers/finalizers,verbs=update
-
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the DemoController object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *DemoControllerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := ctrllog.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// Fetch the DemoController instance
+	demoController := &groupcontrollerv1alpha1.DemoController{}
+	err := r.Get(ctx, req.NamespacedName, demoController)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("Memcached resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get Memcached")
+		return ctrl.Result{}, err
+	}
+
+	// Check if the deployment already exists, if not create a new one
+	found := &appsv1.Deployment{}
+	err = r.Get(ctx, types.NamespacedName{Name: demoController.Name, Namespace: demoController.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new deployment
+		dep := r.deploymentForDemoController(demoController)
+		log.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+
+		err = r.Create(ctx, dep)
+		if err != nil {
+			log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+
+		// Deployment created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get Deployment")
+		return ctrl.Result{}, err
+	}
+
+	// Ensure the deployment size is the same as the spec
+	size := demoController.Spec.Size
+	if *found.Spec.Replicas != size {
+		found.Spec.Replicas = &size
+		err = r.Update(ctx, found)
+		if err != nil {
+			log.Error(err, "Failed to update Deployment", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
+			return ctrl.Result{}, err
+		}
+		// Ask to requeue after 1 minute in order to give enough time for the
+		// pods be created on the cluster side and the operand be able
+		// to do the next update step accurately.
+		return ctrl.Result{RequeueAfter: time.Minute}, nil
+	}
+
+	// Update the DemoController status with the pod names
+	// List the pods for this demoController's deployment
+	podList := &corev1.PodList{}
+	listOpts := []client.ListOption{
+		client.InNamespace(demoController.Namespace),
+		client.MatchingLabels(labelsForDemoController(demoController.Name)),
+	}
+	if err = r.List(ctx, podList, listOpts...); err != nil {
+		log.Error(
+			err,
+			"Failed to list pods",
+			"DemoController.Namespace",
+			demoController.Namespace,
+			"DemoController.Name",
+			demoController.Name,
+		)
+		return ctrl.Result{}, err
+	}
+	podNames := getPodNames(podList.Items)
+
+	// Update status.Nodes if needed
+	if !reflect.DeepEqual(podNames, demoController.Status.Nodes) {
+		demoController.Status.Nodes = podNames
+		err := r.Status().Update(ctx, demoController)
+		if err != nil {
+			log.Error(err, "Failed to update Memcached status")
+			return ctrl.Result{}, err
+		}
+	}
 
 	return ctrl.Result{}, nil
+}
+
+// deploymentForDemoController returns a memcached Deployment object
+func (r *DemoControllerReconciler) deploymentForDemoController(m *groupcontrollerv1alpha1.DemoController) *appsv1.Deployment {
+	ls := labelsForDemoController(m.Name)
+	replicas := m.Spec.Size
+
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image: "nginx:1.14.2",
+						Name:  "nginx",
+						Ports: []corev1.ContainerPort{{
+							ContainerPort: 80,
+						}},
+					}},
+				},
+			},
+		},
+	}
+	// Set Memcached instance as the owner and controller
+	ctrl.SetControllerReference(m, dep, r.Scheme)
+	return dep
+}
+
+// labelsForDemoController returns the labels for selecting the resources
+// belonging to the given memcached CR name.
+func labelsForDemoController(name string) map[string]string {
+	return map[string]string{"app": "nginx", "nginx_cr": name}
+}
+
+// getPodNames returns the pod names of the array of pods passed in
+func getPodNames(pods []corev1.Pod) []string {
+	var podNames []string
+	for _, pod := range pods {
+		podNames = append(podNames, pod.Name)
+	}
+	return podNames
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *DemoControllerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&groupcontrollerv1alpha1.DemoController{}).
+		Owns(&appsv1.Deployment{}).
 		Complete(r)
 }
